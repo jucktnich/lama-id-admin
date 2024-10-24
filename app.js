75b4077d0eec78fabf7a7f1fc90de530dc49ab11
jucktnich <73582096+jucktnich@users.noreply.github.com>
2024-10-24 18:26:36 +0200

    .

--- app.js
+++ app.js
@@ -361,7 +361,7 @@ async function createIDsScreen() {
     groups = groups.filter((group) => group.accepted === group.overall);
     let groupList = [];
     for (let i = 0; i < groups.length; i++) {
-      groupList.push('' + groups[i][0].id);
+      groupList.push('' + groups[i].id);
     }
     await createIDs('1ced281d-8ea0-46e2-a9d7-046be3dd41a9', {}, groupList);
     await createIDs('4cd7f3da-0d2f-4760-9ba1-4bbc1ce62186', {}, groupList);
